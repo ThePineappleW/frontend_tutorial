8abbae2008cbca4211c7b14ef29053da6598dd34
PineappleW <57153066+ThePineappleW@users.noreply.github.com>
2020-12-15 22:37:45 -0500

    began adding api compatibility

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//hello world
 const title = <h1>Hello, World!</h1>;
+
+//button for functionality
+//const button =<Button variant="primary">Click to get a random Star Wars Character</Button>;
+
 
 ReactDOM.render(title, document.getElementById('root'));
