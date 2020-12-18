a458713b05fbb200bd33b4d5a4b1224ba7a6f593
PineappleW <57153066+ThePineappleW@users.noreply.github.com>
2020-12-18 00:18:47 -0500

    began adding fetch capabilities (DOES NOT WORK)

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,11 +1,74 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//hello world
-const title = <h1>Hello, World!</h1>;
+//I have not yet implemented the input fields,  they are currently just placeholders.
 
-//button for functionality
-//const button =<Button variant="primary">Click to get a random Star Wars Character</Button>;
+var submitInfo = function(){
+    var response = {
+        method: 'POST',
+        headers: new Headers({
+            'Content-Type': 'application/x-www-form-urlencoded'
+          }),
+        body: JSON.stringify({
+            name: 'France',
+            capital: 'Paris',
+            continent: 'Europe',
+            hdi: '0.950'
+        }),
+        mode: 'no-cors'
+    };
+
+    fetch( "http://localhost:3000/api/countries/", response)
+        .then(response => response.json())
+        .then(responseJson => {
+            alert(responseJson.message);
+        })
+        .catch((error) => {
+            console.error(error);
+          });
+}
+
+var getAllCountries = function(){
+    fetch( "http://localhost:3000/api/countries/" )
+    .then( (response) => response.json() )
+    .then( (responseJson) => {
+        alert(responseJson)
+    })
+    .catch((error) => {
+        console.error(error);
+      });
+}
+
+const username = <input type="text" placeholder="Username" />;
+const password = <input type="text" placeholder="Password" />
+const countryInput = {
+    name: <input type="text" placeholder="Name" />,
+    capital: <input type="text" placeholder="Capital" />,
+    continent: <input type="text" placeholder="Continent" />,
+    hdi: <input type="number" placeholder="HDI" />
+}
 
 
-ReactDOM.render(title, document.getElementById('root'));
+const body = <div>
+                <h1>CountryDB</h1>
+                <br />
+                {username}
+                <br />
+                {password}
+                <br />
+                <h2> Input country data</h2>
+                {countryInput.name}
+                {countryInput.capital}
+                <br />
+                {countryInput.continent}
+                {countryInput.hdi}
+                <br />
+                <br />
+                <button onClick={submitInfo}>Submit</button>
+                <br />
+                <br />
+                <button onClick={getAllCountries}>Get all loaded countries</button>
+             </div>;
+
+//render everything
+ReactDOM.render(body, document.getElementById('root'));
